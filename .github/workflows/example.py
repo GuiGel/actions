e24da9d10db0e492fa3e81a7a152bc148df39740
GuiGel <44615205+GuiGel@users.noreply.github.com>
2022-07-18 14:45:35 +0200

    :construction: Update put good secret token name

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -2,7 +2,7 @@ import os
 
 def main():
   print("Hello from GitHub Actions!")
-  token = os.environ.get("BEGIN_ACTION_SECRET_TOKEN")
+  token = os.environ.get("BEGIN_WITH_ACTION_SECRET_TOKEN")
   if not token:
     raise RunTimeError("BEGIN_ACTION_SECRET_TOKEN env var is not set!")
    print("All good! We found our env var")
