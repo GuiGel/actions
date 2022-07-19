a014df794deb98b851666275aadb3ca1a5209e39
GuiGel <44615205+GuiGel@users.noreply.github.com>
2022-07-19 08:34:52 +0200

    :construction: Update print python version

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -2,6 +2,7 @@ import os
 
 def main():
   print("Hello from GitHub Actions!")
+  print(f"We are using python version {python.__version__}")
   token = os.environ.get("BEGIN_WITH_ACTION_SECRET_TOKEN")
   if not token:
     raise RunTimeError("BEGIN_ACTION_SECRET_TOKEN env var is not set!")
